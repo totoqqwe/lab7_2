87d0825170fe7711e2e1b89bdc8eac78f8912a46
totoqqwe <anton.stepanchenko14@gmail.com>
2023-11-23 20:21:34 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository<Person> personRepository = new Repository<Person>();
+
+            personRepository.Add(new Person("John", 25));
+            personRepository.Add(new Person("Alice", 30));
+            personRepository.Add(new Person("Bob", 22));
+
+            List<Person> result = personRepository.Find(p => p.Age > 25);
+
+            Console.WriteLine("Persons older than 25:");
+            foreach (Person person in result)
+            {
+                Console.WriteLine($"{person.Name}, {person.Age} years old");
+            }
+        }
+    }
+}
---
+++ ConsoleApp13/Repository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Repository<T>
+    {
+        public delegate bool Criteria<T>(T item);
+
+        private List<T> items;
+
+        public Repository()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public List<T> Find(Criteria<T> criteria)
+        {
+            List<T> result = new List<T>();
+
+            foreach (T item in items)
+            {
+                if (criteria(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+    }
+}
